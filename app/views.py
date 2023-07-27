52caa25beb2f9102dab1857a21b74043c11da0e1
anushapanchananam <anushapanchananam08@gmail.com>
2023-07-27 12:06:51 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def web(request):
-    d = {'name': 'VYSHNAVI', 'age': 18}
+    d = {'name': 'VYSHNAVI', 'age': 8}
     return render(request, 'web.html', context=d)
