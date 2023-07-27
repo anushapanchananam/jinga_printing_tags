097ac2a220b7bdcf0f7c036f341fcedc737cc8f1
anushapanchananam <139953468+anushapanchananam@users.noreply.github.com>
2023-07-27 23:25:21 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def web(request):
-    d = {'name': 'VYSHNAVI', 'age': 8}
+    d = {'name': 'VYSHNAVI', 'age': 18}
     return render(request, 'web.html', context=d)
